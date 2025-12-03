2edf20fbe03ff27e2198e0fd30ad2911335ba20e
Eugene O. (id753) <mimywhy@gmail.com>
2025-12-03 12:09:19 +0200

    fin

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import "./App.css";
+import css from "../src/styles/App.module.css";
+import CafeInfo from "./CafeInfo/CafeInfo";
+import { useState } from "react";
+import type { Votes } from "./types/votes";
+import type { VoteType } from "./types/votes";
+import VoteOptions from "./VoteOptions/VoteOptions";
+import VoteStats from "./VoteStats/VoteStats";
+import Notification from "./Notification/Notification";
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prev) => ({ ...prev, [type]: prev[type] + 1 }));
+  };
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const canReset = totalVotes > 0;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={canReset}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Notification/Notification.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Notification.module.css";
+
+const Notification = () => {
+  return <p className={styles.message}>No feedback yet</p>;
+};
+
+export default Notification;
